b591858bd548207541bb51c0c08f26cb4b25eb45
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-09 09:39:00 +0300

    commit tree 57a053c9598413b0d2299b5e040faa1cd91641fa

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+//select
+//first two
+function returnFirstTwoDrivers(array)
+{
+    return array.slice(0,2)
+}
+//last two
+function returnLastTwoDrivers(array)
+{
+    return array.slice(2,4)
+}
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+console.log(selectingDrivers[1])
+
+function createFareMultiplier(value)
+{
+    return  function fareMultiplier()
+    {
+        return value*value;
+    }
+}
+function fareDoubler(fare){
+    return fare*2;
+}
+function fareTripler(fare)
+{
+    return fare*3;
+}
+function selectDifferentDrivers(arrayOfDrivers,aFunction)
+{
+    return aFunction(arrayOfDrivers);
+}
+
+
+
